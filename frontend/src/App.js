633912b54ddb24fb340f9cbed91541f4bb77bd8d
seawavve <hanhee.sw@gmail.com>
2022-01-04 01:42:16 +0900

    create FE demo

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Template from './components/mypage';
+class App extends Component {
+  render() {
+    return (
+      <Template>
+        App
+      </Template>
+    );
+  }
+}
+
+export default App;
