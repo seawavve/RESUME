6a80b090f8569cf16b32829f0ad00c33b7d21aff
seawavve <hanhee.sw@gmail.com>
2022-01-04 02:16:46 +0900

    make backend link to frontend console

--- frontend/src/components/mypage.js
+++ frontend/src/components/mypage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './mypage.css';
 
+fetch("http://localhost:8081/mypage").then((response) =>
+        console.log(response));
+        
 const Template = ({form, children}) => {
   return (
     <main className="template">
       <div className="title">
         백앤드 내용
+        
       </div>
     </main>
   );
